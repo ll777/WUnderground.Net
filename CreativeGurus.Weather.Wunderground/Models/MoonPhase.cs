77b832e4027603f7c674230091f154b9b180e533
ll777 <123654@binkmail.com>
2017-06-11 22:29:48 -0300

    Create MoonPhase.cs

--- CreativeGurus.Weather.Wunderground/Models/MoonPhase.cs
+++ CreativeGurus.Weather.Wunderground/Models/MoonPhase.cs
@@ -3,6 +3,8 @@
     public class MoonPhase
     {
         public string AgeOfMoon { get; set; }
+        public string PhaseOfMoon { get; set; }
+        public string Hemisphere { get; set; }
         public Time Current_Time { get; set; }
         public string PercentIlluminated { get; set; }
         public Time Sunrise { get; set; }
